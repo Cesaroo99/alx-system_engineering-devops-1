4ad4cfa9a9be66731bb07706bb8c88510253ee76
root <memolicesar1@gmail.com>
2023-02-28 15:13:08 -0800

    tasks

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -50,5 +50,5 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
                 return
             instances = sorted(instances.items(), key=lambda kv: (-kv[1]; kv[0]))
             [print("{}: {}".format(k, v)) for k, v in instances]
-        else
-        count_words(subreddit, word_list, instances, after, count)
+        else:
+            count_words(subreddit, word_list, instances, after, count)
